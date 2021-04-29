d0966bbac45a17b5ae56b564e03f447ccac2d1d6
cit-arceo <citlalliarceo@gmail.com>
2021-04-29 18:00:22 -0500

    login and register

---
+++ src/components/Login.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import '../styles/auth.css';
+
+export const Login = () => {
+    return (
+        <>
+        <header>Granja Domotica</header>
+            <div className="main">
+                <form >
+                    <h3 className="titulo">Login</h3>
+                        <input 
+                            type="text"
+                            placeholder="Usuario"
+                            name="user"
+                            className="input"
+                            autoComplete="off"
+                        />
+
+                        <input 
+                            type="password"
+                            placeholder="Password"
+                            name="password"
+                            className="input"
+                        />
+
+
+                        <button
+                            type="submit"
+                            className="btn btn-primary btn-block"
+                        >
+                        Iniciar
+                        </button>
+                </form>
+        </div>
+        </>
+    )
+}
